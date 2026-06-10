58435ab69b3fc3477b42b1e0c05bd7ddddf7f192
Konstantin Ryazanov <konstantin21_arx@bk.ru>
2026-06-11 00:38:45 +0700

    in progress..

--- packages/shared/src/i18n/index.ts
+++ packages/shared/src/i18n/index.ts
@@ -4,16 +4,22 @@ import thBase from './th.json'
 import ruAbout from './about/ru.json'
 import enAbout from './about/en.json'
 import thAbout from './about/th.json'
+import ruProfile from './profile/ru.json'
+import enProfile from './profile/en.json'
+import thProfile from './profile/th.json'
 
 export const ru = {
   ...ruBase,
   about: ruAbout,
+  profile: ruProfile,
 }
 export const en = {
   ...enBase,
   about: enAbout,
+  profile: enProfile,
 }
 export const th = {
   ...thBase,
   about: thAbout,
+  profile: thProfile,
 }
