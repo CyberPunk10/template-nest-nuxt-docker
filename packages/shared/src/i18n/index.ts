1a1e7cc46af8071e16c096a92c200d12d898a936
Konstantin Ryazanov <konstantin21_arx@bk.ru>
2026-07-12 21:55:52 +0700

    remove About page from frontend

--- packages/shared/src/i18n/index.ts
+++ packages/shared/src/i18n/index.ts
@@ -1,19 +1,13 @@
 import ruBase from './ru.json'
 import enBase from './en.json'
 import thBase from './th.json'
-import ruAbout from './about/ru.json'
-import enAbout from './about/en.json'
-import thAbout from './about/th.json'
 
 export const ru = {
   ...ruBase,
-  about: ruAbout,
 }
 export const en = {
   ...enBase,
-  about: enAbout,
 }
 export const th = {
   ...thBase,
-  about: thAbout,
 }
